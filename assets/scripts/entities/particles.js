35d095d5cde41b8be4848c43969c05e25514c5ba
alternativelowtier <marian06santana@gmail.com>
2026-05-04 14:24:36 -0400

    Update particles.js

--- assets/scripts/entities/particles.js
+++ assets/scripts/entities/particles.js
@@ -17,4 +17,15 @@ else if (this.behavior === 'yuta_slash') { ctx.save(); ctx.translate(this.x, thi
 else if (this.behavior === 'black_flash') { ctx.fillStyle = '#000'; ctx.strokeStyle = '#ff0000'; ctx.lineWidth = 1; let size = (this.life > 10 ? 6 : 4) * pScale; ctx.fillRect(this.x, this.y, size, size); ctx.strokeRect(this.x, this.y, size, size); } 
 else if (this.behavior === 'dash_trail') { ctx.fillStyle = this.color; ctx.globalAlpha = pScale * 0.4; ctx.fillRect(this.x - 8, this.y - 22, 16, 22); } 
 else if (this.behavior === 'dust') { ctx.fillStyle = this.color; ctx.fillRect(this.x, this.y, 2, 2); } 
-else if (this.behavior === 'rct_heal') { let size = 1 + pScale*3; ctx.fillStyle = Math.random() > 0.5 ? '#ff85a1' : '#ffcad4'; ctx.globalCompositeOperation = 'lighter'; ctx.beginPath(); ctx.arc(this.x
+else if (this.behavior === 'rct_heal') { let size = 1 + pScale*3; ctx.fillStyle = Math.random() > 0.5 ? '#ff85a1' : '#ffcad4'; ctx.globalCompositeOperation = 'lighter'; ctx.beginPath(); ctx.arc(this.x, this.y, size, 0, Math.PI * 2); ctx.fill(); ctx.globalCompositeOperation = 'source-over'; }
+else if (this.behavior === 'black_flash_lightning') { ctx.save(); ctx.translate(this.x, this.y - 15); ctx.lineCap = "round"; ctx.lineJoin = "round"; for (let pts of this.points) { ctx.beginPath(); ctx.strokeStyle = '#ff0000'; ctx.lineWidth = 5 + Math.random()*4; ctx.moveTo(pts[0].x, pts[0].y); for (let i=1; i<pts.length; i++) ctx.lineTo(pts[i].x + (Math.random()-0.5)*4, pts[i].y + (Math.random()-0.5)*4); ctx.stroke(); ctx.beginPath(); ctx.strokeStyle = '#000000'; ctx.lineWidth = 2 + Math.random()*2; ctx.moveTo(pts[0].x, pts[0].y); for (let i=1; i<pts.length; i++) ctx.lineTo(pts[i].x, pts[i].y); ctx.stroke(); } ctx.restore(); } 
+else { let size = (this.life > 15 ? 4 : 2) + (pScale * 2); ctx.fillStyle = this.color; ctx.fillRect(this.x, this.y, size, size); } ctx.globalAlpha = 1.0;
+}
+}
+
+function spawnHitSparks(x, y, color, isBig, isSlash = false, isBlackFlash = false, isYutaSlash = false) {
+if (isBlackFlash) { gameEngine.particles.push(new ParticleEffect(x, y, 0, 0, '', 'black_flash_lightning')); } 
+else if (isYutaSlash) { for (let i = 0; i < (isBig ? 3 : 1); i++) { gameEngine.particles.push(new ParticleEffect(x + (Math.random()-0.5)*20, y - 15 + (Math.random()-0.5)*20, 0, 0, '', 'yuta_slash')); } }
+else if (isSlash) { for (let i = 0; i < (isBig ? 3 : 1); i++) { gameEngine.particles.push(new ParticleEffect(x + (Math.random()-0.5)*20, y - 15 + (Math.random()-0.5)*20, 0, 0, '', 'slash')); } } 
+else { let count = isBig ? 20 : 6; for (let i = 0; i < count; i++) { gameEngine.particles.push(new ParticleEffect(x, y - 15, (Math.random() - 0.5) * (isBig ? 20 : 10), (Math.random() - 1) * 7, color)); } }
+}
